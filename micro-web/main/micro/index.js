feadd93578767fa438dc5f8b731cc0880e2399ae
赵 安东 <zhaoandong@bobfintech.com.cn>
2022-09-21 22:09:30 +0800

    fix: 非IE

--- micro-web/main/micro/index.js
+++ micro-web/main/micro/index.js
@@ -1,1 +1,7 @@
 export { registerMicroApps, start } from './start'
+
+/* var isIE = document.documentMode || +(navigator.userAgent.match(/MSIE (\d+)/) && RegExp.$1)
+var isEDGE = navigator.userAgent.indexOf('Edge') > -1
+if (isIE || isEDGE) {
+  alert('建议使用Chrome浏览器访问，以便获取最佳用户体验。')
+} */
